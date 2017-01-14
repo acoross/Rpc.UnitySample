adab38864d1d4abb76f3c7a43f38a968da9efbe0
Ilhwan Shin <acoross@gmail.com>
2017-01-14 14:50:04 +0900

    chat button

--- Assets/MyGui.cs
+++ Assets/MyGui.cs
@@ -16,10 +16,19 @@ public class MyGui : MonoBehaviour {
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(10, 70, 50, 50), "Button"))
+        if (GUI.Button(new Rect(10, 70, 50, 50), "Login"))
         {
+			Debug.Log ("login clicked");
             var rpc = GetComponent<MySocket>();
-            rpc.Login("acoross", "");
+            bool ok = rpc.Login("acoross", "");
+			Debug.Log ("login" + ok.ToString());
         }
+
+		if (GUI.Button(new Rect(10, 130, 50, 50), "Chat"))
+		{
+			Debug.Log ("chat clicked");
+			var rpc = GetComponent<MySocket>();
+			rpc.Chat ("unity", "chat");
+		}
     }
 }
